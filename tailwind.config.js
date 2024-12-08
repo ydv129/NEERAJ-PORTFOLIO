1455316f67cb7cfa0dae19e343a28e974ea412ab
NICK <169161801+ydv129@users.noreply.github.com>
2024-12-08 13:58:08 +0000

    fst

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      transitionTimingFunction: {
+        jump: "cubic-bezier(0.68, -0.55, 0.265, 1.55)",
+      },
+    },
   },
   plugins: [],
-}
-
+};
